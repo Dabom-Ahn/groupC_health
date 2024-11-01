9ff8fa33a78fa18bd7a8fe7b78f06d7c3af6fb74
Dabom Ahn <900428star@naver.com>
2024-11-01 10:07:26 +0900

    end

--- src/components/main/pages/MainNoticeBanner.jsx
+++ src/components/main/pages/MainNoticeBanner.jsx
@@ -1,4 +1,5 @@
 import bannerData from '../../../data/mainnoticedata';
+import { Link } from 'react-router-dom';
 
 export default function MainNoticeBanner() {
 	return (
@@ -7,12 +8,16 @@ export default function MainNoticeBanner() {
 				<div className='banneritem'>
 					{bannerData.map(bannerData => (
 						<div key={bannerData.id} className={`banner${bannerData.id}`}>
-							<img src={bannerData.imgSrc} alt={bannerData.alt} />
+							<Link to={bannerData.link} className='board1'>
+								<img src={bannerData.imgSrc} alt={bannerData.alt} />
+							</Link>
 						</div>
 					))}
 				</div>
 				<div className='board'>
-					<img src='../../pic/sub/noticeBanner/mainfix.png' alt='게시판바로가기' className='board1' />
+					<Link to='https://www.nhis.or.kr/nhis/policy/wbhadd04300m01.do' className='board1'>
+						<img src='../../pic/sub/noticeBanner/mainfix.png' alt='게시판 바로가기' />
+					</Link>
 				</div>
 			</div>
 		</section>
--- src/data/mainnoticedata.js
+++ src/data/mainnoticedata.js
@@ -1,6 +1,16 @@
 const bannerData = [
-	{ id: 1, imgSrc: '../../pic/sub/noticeBanner/1.png', alt: '배너 1 설명', link: '#' },
-	{ id: 2, imgSrc: '../../pic/sub/noticeBanner/2.png', alt: '배너 2 설명', link: '#' }
+	{
+		id: 1,
+		imgSrc: '../../pic/sub/noticeBanner/1.png',
+		alt: '배너 1 설명',
+		link: 'https://www.longtermcare.or.kr/npbs/main/images/common/main202309_2.pdf'
+	},
+	{
+		id: 2,
+		imgSrc: '../../pic/sub/noticeBanner/2.png',
+		alt: '배너 2 설명',
+		link: 'https://www.longtermcare.or.kr/npbs/contents/np/popup/20240913_pop01.html'
+	}
 ];
 
 export default bannerData;
